cccf47d577ffa22d3aa35dd8104ca4747eb494f8
Chi-Hsien Wu <chwu891@gmail.com>
2024-07-26 13:30:30 -0700

    Initial Commit

---
+++ FirebasePractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebasePractice
+//
+//  Created by Chi-Hsien Wu on 7/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebasePractice/FirebasePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebasePracticeApp.swift
+//  FirebasePractice
+//
+//  Created by Chi-Hsien Wu on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebasePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
